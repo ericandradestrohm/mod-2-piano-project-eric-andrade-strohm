a4261967461c0d3afbac96c9423fd89d644e6430
Eric Andrade-Strohm <eric.j.strohm@gmail.com>
2024-07-13 10:47:39 -0700

    Added keycodes and functioning javascript eventlisteners

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const pianoKeys = document.querySelector('.piano-keys');
 
 
 function playKey(e) {
-    console.log(e);
+    console.log(e.keyCode);
+    const keyDiv = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (keyDiv){
+        keyDiv.classList.add('playing');
+    }
+}
+
+function unplayKey(e) {
+    console.log(e.keyCode);
+    const keyDiv = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (keyDiv){
+        keyDiv.classList.remove('playing');
+    }
 }
 
 
 window.addEventListener('keydown',playKey);
+window.addEventListener('keyup',unplayKey);
